513ca10e77cb069cf4de0d397e1b40eea6bfa600
AlbertSantamaria <santamaria.lara@gmail.com>
2014-10-08 21:05:33 +0200

    Iniciando Repositorio

---
+++ ClasesEsenciales3/src/main/java/edu/upc/eetac/dsa/asantamaria/ClasesEsenciales3/App.java
@@ -0,0 +1,13 @@
+package edu.upc.eetac.dsa.asantamaria.ClasesEsenciales3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
